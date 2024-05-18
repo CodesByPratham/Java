8d21f241f75fed348d4eee8f4aea674fda09ff0f
Prathamr-codes <prathamrathod9601@gmail.com>
2024-05-18 12:32:18 +0530

    Modified the programs

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,27 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ans[] = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
     }
 }
 
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+
+        int[] arr = { 3, 2, 4 };
+        System.out.println(Arrays.toString(sl.twoSum(arr, 6)));
+
+    }
+}
