fa3aa96664865ff2c99ee4d9625f74be43353a3f
Prathamr-codes <prathamrathod9601@gmail.com>
2024-07-23 11:01:22 +0530

    Deleted Solved Problems Folder

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World...");
+        int a[] = { 1, 2, 3, 4, 5 };
+
+        increment(a);
+
+        System.out.println(Arrays.toString(a));
     }
 
+    private static void increment(int[] a) {
+        a[2] = 99;
+        // System.out.println(a);
+    }
 }
